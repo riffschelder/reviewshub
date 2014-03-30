7d3b4f149a1028bb68d917fec2bc3a89a0f170ee
Riff Schelder <riff@schelder.com>
2014-03-30 14:29:14 -0500

    set ALLOWED_HOSTS for production

--- reviewsHub/reviewsHub/settings/production.py
+++ reviewsHub/reviewsHub/settings/production.py
@@ -1,7 +1,7 @@
 DEBUG = False
 TEMPLATE_DEBUG = False
 SECRET_KEY = '$d416i%#@eqim_ms34y42jy%7-+(ml7*7iz8l!w7*7h%et!i3l'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.schelder.com', '.schelder.com.']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
