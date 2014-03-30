3ae7f2ac7e794b2ea155469275e8e064f32f6965
Riff Schelder <riff@schelder.com>
2014-03-30 14:25:08 -0500

    split base and local/production settings

---
+++ reviewsHub/reviewsHub/settings/production.py
@@ -0,0 +1,14 @@
+DEBUG = True
+TEMPLATE_DEBUG = True
+SECRET_KEY = '$d416i%#@eqim_ms34y42jy%7-+(ml7*7iz8l!w7*7h%et!i3l'
+ALLOWED_HOSTS = []
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',                      
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
