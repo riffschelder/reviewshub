5f9293843a04a6323b8d8f3531d5a40b52e68ef6
Riff Schelder <riff@schelder.com>
2014-03-30 15:31:09 -0500

    turn on production debug

--- reviewsHub/reviewsHub/settings/production.py
+++ reviewsHub/reviewsHub/settings/production.py
@@ -1,5 +1,5 @@
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = True
 SECRET_KEY = '$d416i%#@eqim_ms34y42jy%7-+(ml7*7iz8l!w7*7h%et!i3l'
 ALLOWED_HOSTS = ['.schelder.com', '.schelder.com.']
 DATABASES = {
