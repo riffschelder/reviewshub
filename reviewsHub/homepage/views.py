b360497e752826a2ee3bca44df1e25622c1c0aec
Riff Schelder <riff@schelder.com>
2014-05-03 14:07:09 -0500

    abandon amazon for best buy

--- reviewsHub/homepage/views.py
+++ reviewsHub/homepage/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Best Hub for Amazon Reviews.")
+    return HttpResponse("Best Hub for Best Buy Reviews.")
 
